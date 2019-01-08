6de1ef4cc3b94e646e7be8127bad5b0b3b96ada6
ssaanvi <league@WTS22.attlocal.net>
2019-01-07 16:10:49 -0800

    1/7/19

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -4,22 +4,30 @@ package extra;
 
 import javax.swing.JOptionPane;
 
-public class BananaQuiz
-{
-	
-	public static void main(String[] args)
-	{
-		//1. ask the user if they like bananas
-		//2. if they say no, 
-			//tell them they are crazy 
-			//and end quiz
-		//3. if they say yes
-		//	ask them what is their favorite hobby
-		//	show a pop up that says "<your hobby> is much better with bananas!"
+public class BananaQuiz {
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
-	
+	public static void main(String[] args) {
+		// 1. ask the user if they like bananas
+		String name = JOptionPane.showInputDialog("Dear user, what is your name?");
+		String like = JOptionPane
+				.showInputDialog("Okay. " + name + ", let's start the banana quiz!!! Do you like banana's?");
+		// 2. if they say no,
+		// tell them they are crazy
+		if (like.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "You are crazy.");
+
+		}
+		// and end quiz
+		// 3. if they say yes
+		// ask them what is their favorite hobby
+		if (like.equalsIgnoreCase("yes")) {
+			String hobby = JOptionPane.showInputDialog(null, "Okay. " + name + ", What is your favorite hobby?");
+			JOptionPane.showMessageDialog(null, "Your hobby, " + hobby + ", is much better with bananas!!!");
+		}
+		// show a pop up that says "<your hobby> is much better with bananas!"
+
+		// 4. OPTIONAL: if they say something other than “yes�? or “no�?
+		// show a pop up that says “You are bananas!�?
+
 	}
-
 }
---
+++ src/extra/ObedientRobot.java
@@ -0,0 +1,38 @@
+package extra;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class ObedientRobot {
+	static Robot Robot = new Robot();
+	public static void main(String[] args) {
+
+		
+		
+		Robot.penDown();
+		Robot.setRandomPenColor();
+		Robot.setPenWidth(5);
+		Robot.setSpeed(22);
+		for (int i = 0; i < 4; i++) {
+		Robot.move(100);
+		Robot.turn(90);
+		}}
+	
+	Robot.penDown();
+	Robot.setRandomPenColor();
+	Robot.setPenWidth(5);
+	Robot.setSpeed(22);
+	for (int i = 0; i < 4; i++) {
+	Robot.move(100);
+	Robot.turn(66)
+
+	
+
+	static void drawSquare () {
+		Robot.penDown();
+	Robot.setRandomPenColor();
+	Robot.setPenWidth(5);
+	Robot.setSpeed(22);
+	for (int i = 0; i < 4; i++) {
+	Robot.move(100);
+	Robot.turn(90);
+	}}}
