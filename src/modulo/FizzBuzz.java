847e04cd8ed8423cc96e35ae71899da9827dde23
amanddali <league@WTS22.attlocal.net>
2019-01-14 16:18:06 -0800

    1/14/19

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,29 +3,40 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
-* Fizz Buzz
-* 
-* In this project, we're going to build FizzBuzz. It's a children's game where
-* you count from 1 to 20. Easy, right? Here's the catch: instead of saying
-* numbers divisible by 3, say "Fizz". And instead of saying numbers divisible
-* by 5, say "Buzz". For numbers divisible by both 3 and 5, say "FizzBuzz".
-* 
-* So the rules are:
-* 		Any number divisible by 3 is replaced by the word fizz 
-* 		Any number divisible by 5 is replaced by the word buzz. 
-* 		Numbers divisible by both 3 and 5 become fizzbuzz.
-* 
-* Print your results to the console, or using JOptionPane if you like.
-* 
-* If your code is correct, the output will be:
-*  
-* 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
-* 
-**/
+ * Fizz Buzz
+ * 
+ * In this project, we're going to build FizzBuzz. It's a children's game where
+ * you count from 1 to 20. Easy, right? Here's the catch: instead of saying
+ * numbers divisible by 3, say "Fizz". And instead of saying numbers divisible
+ * by 5, say "Buzz". For numbers divisible by both 3 and 5, say "FizzBuzz".
+ * 
+ * So the rules are: Any number divisible by 3 is replaced by the word fizz Any
+ * number divisible by 5 is replaced by the word buzz. Numbers divisible by both
+ * 3 and 5 become fizzbuzz.
+ * 
+ * Print your results to the console, or using JOptionPane if you like.
+ * 
+ * If your code is correct, the output will be:
+ * 
+ * 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
+ * 
+ **/
 
 public class FizzBuzz {
+	public static void main(String[] args) {
+      
+		String name = JOptionPane.showInputDialog("Dear user, what is your name?");
+		JOptionPane.showMessageDialog(null, "Okay. " + name + ", let's start the Fizz Buzz game!");
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i);
 
+			if (i % 3 == 0) {
+				System.out.println("Fizz");
+			}
+		}
+		
+	}
 }
-
